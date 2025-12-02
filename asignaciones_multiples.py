d8f2905c4623856f02f183345af5320dd5b15bb1
Camilo Diaz <camilo20263@gmail.com>
2025-12-02 15:35:46 -0500

    Asignaciones múltiples

---
+++ asignaciones_multiples.py
@@ -0,0 +1,15 @@
+
+# Asignaciones múltiples
+carro1,carro2,carro3="Mazda","Toyota","Honda"
+print(carro3) # Imprime: Honda
+
+# Intercambio de valores entre variables
+
+print(carro2+" es mejor que "+carro1) # Imprime: Toyota es mejor que Mazda
+
+
+a=5
+b=5
+print(a+b) # Imprime: 10
+
+print(a, b) # Imprime: 5 5
