7afced6347e204231852e64853137623b6c7755b
Camilo Diaz <camilo20263@gmail.com>
2025-12-02 15:24:35 -0500

    variables validas y no validas

--- variables.py
+++ variables.py
@@ -1,3 +1,15 @@
 x = "Esto es una variable de tipo string"
 
 print (x) 
+# Válidos
+texto = "Esta es una variable"
+mi_variable = "con guion bajo"
+_mi_variable = "comienza con guion bajo"
+miVariable = "mixto (estilo *camel case*)"
+MI_VARIABLE = "todo en mayúscula"
+mi_variable2 = "números al final"
+
+# Inválidos (no usar)
+# 2variable = "no puede iniciar con número"
+# mi-variable = "no se permite guion medio"
+# mi variable = "no se permiten espacios"
