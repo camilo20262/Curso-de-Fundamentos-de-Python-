2ccadc876ba0892579489940db9dd8275ff422bd
Camilo Diaz <camilo20263@gmail.com>
2025-12-02 18:41:19 -0500

    datos numericos

---
+++ numeros.py
@@ -0,0 +1,14 @@
+x = 1
+y = 2.5
+z = 1j
+
+#mostrar tipo de dato
+print(type(x))
+print(type(y))
+print(type(z))
+ 
+# cambiar tipo de dato x INT a FLOAT 
+xf = float(x)
+print(type(xf))
+#muestra en pantalla es 1.0
+print(xf)
