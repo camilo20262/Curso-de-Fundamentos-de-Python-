eabe08f9fbcc54654404a67cfce9a9a7b2b15197
Camilo Diaz <camilo20263@gmail.com>
2025-12-02 16:52:38 -0500

    tipos de datos

---
+++ tipos_datos.py
@@ -0,0 +1,48 @@
+texto1 = 'este es un texto'
+texto2 = "este es un texto"
+texto3 = '''este es un texto'''
+texto4 = """este es un texto"""
+
+print(texto1)
+print(texto2)
+print(texto3)
+print(texto4)
+
+
+
+A = 42          # entero
+B = 3.14        # flotante (usa punto, no coma)
+C = 5 + 2j      # complejo, parte imaginaria con J
+
+print(A)
+print(B)
+print(C)
+
+
+
+
+
+lista = [0, 1, 2, 3, 4, 5]      # ordenada y mutable
+
+# tupla ordenada e inmutable
+tupla = ('A', 'B', 'C')
+
+
+
+
+
+diccionario = {
+    'nombre': 'Ada',
+    'edad': 36
+}
+
+
+
+conjunto = {1, 1, 2, 2, 3}
+# Output esperado: {1, 2, 3}
+
+
+
+booleano_verdadero = True
+booleano_falso = False
+
