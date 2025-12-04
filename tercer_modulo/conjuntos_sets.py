58ffea5df28a2995cb1356bb3248fec1a1039f28
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 12:20:26 -0500

    diferentes tipos de datos en un conjunto

--- tercer_modulo/conjuntos_sets.py
+++ tercer_modulo/conjuntos_sets.py
@@ -7,3 +7,8 @@ print(len(frutas))   # 3
 # ciclor sobre un conjunto
 for item in frutas:
     print(item)  # Puede salir en cualquier orden.
+
+# diferentes tipos de datos en un conjunto
+conjunto = {"Python", 156, True}
+print(conjunto)      # Orden no garantizado.
+print(type(conjunto))#
