f8c6d2161a27f250f7f1d1b3e738dc6067970a02
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 12:17:12 -0500

    Definición de un conjunto (set)

---
+++ tercer_modulo/conjuntos_sets.py
@@ -0,0 +1,5 @@
+# Definición de un conjunto (set)
+frutas = {"manzana", "naranja", "mandarina", "naranja"}
+print(frutas)        # {'manzana', 'naranja', 'mandarina'} (el duplicado se ignora)
+print(type(frutas))  # 
+print(len(frutas))   # 3
