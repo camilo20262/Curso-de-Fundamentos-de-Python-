5ea41347f72fbb52920d9ae68c52bc17ceb80ec7
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 11:42:20 -0500

    Definición de una tupla

---
+++ tercer_modulo/tuplas.py
@@ -0,0 +1,8 @@
+# Definición de una tupla
+tecnologias = ("Python", "JavaScript", "Go")
+print(tecnologias)                 # ('Python', 'JavaScript', 'Go')
+print(tecnologias[1])              # 'JavaScript'
+
+#contar elementos
+tecnologias = ("Python", "JavaScript", "Go", "Python")
+print(len(tecnologias))            # 4
