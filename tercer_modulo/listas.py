9300cb3202a0e9e75e692195c12aa65a62cca8fc
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 11:13:38 -0500

    modificar y duplicados

--- tercer_modulo/listas.py
+++ tercer_modulo/listas.py
@@ -1,5 +1,16 @@
 
 # Listas en Python
 # Definición de una lista
-frutas=["mango","pera", "manzana"]
-print(frutas)
+frutas = ["manzana", "naranja", "mandarina"]
+print(frutas)              # ['manzana', 'naranja', 'mandarina']
+print(type(frutas))        # 
+print(frutas[1])           # 'naranja' (índices desde 0)
+
+# Modificar por índice (mutabilidad)
+frutas[1] = "banana"
+print(frutas[1])           # 'banana'
+print(frutas)              # ['manzana', 'banana', 'mandarina']
+
+# Duplicados
+frutas.append("naranja")   # ahora hay otra 'naranja' al final
+print(frutas)              # ['manzana', 'banana', 'mandarina', 'naranja']
