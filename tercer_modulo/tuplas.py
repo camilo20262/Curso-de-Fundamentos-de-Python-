568b5b15359114831f1618636b7ba2949b32ddea
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 11:45:12 -0500

    diferentes tipos de datos en una tupla

--- tercer_modulo/tuplas.py
+++ tercer_modulo/tuplas.py
@@ -6,3 +6,18 @@ print(tecnologias[1])              # 'JavaScript'
 #contar elementos
 tecnologias = ("Python", "JavaScript", "Go", "Python")
 print(len(tecnologias))            # 4
+
+# Tipo de dato
+tecnologias = ("Python", "JavaScript", "Go")
+print(type(tecnologias))           #  tupla
+
+fruta = ("manzana")
+print(type(fruta))                 # string
+
+fruta = ("manzana",)
+print(type(fruta))                 #tupla
+
+# diferentes tipos de datos en una tupla
+tupla = ("Python", 5, True)
+print(tupla)                       # ('Python', 5, True)
+print(type(tupla))                 #
